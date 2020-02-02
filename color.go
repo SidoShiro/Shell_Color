b1b9eb4911e9699ae51b278ffb97b3c0a4c0ae87
sidore_m <marc@sidorenko.fr>
2020-02-02 17:47:54 +0100

    Try better color function organization

--- color.go
+++ color.go
@@ -6,6 +6,7 @@ import (
 )
 
 const (
+	whiteShellColorNum  = 30
 	redShellColorNum    = 31
 	greenShellColorNum  = 32
 	cyanShellColorNum   = 36
@@ -13,42 +14,50 @@ const (
 	blueShellColorNum   = 34
 )
 
+const start string = "\033[0;"
+const end string = "\033[0m"
+
+func Output(color int, str string) string {
+	ss := "\033[0;" + strconv.Itoa(color) + "m" + str + "\033[0m"
+	return ss
+}
+
 func GreenPrintln(str string) {
-	fmt.Println("\033[0;" + strconv.Itoa(greenShellColorNum) + "m" + str + "\033[0m")
+	fmt.Println(Output(greenShellColorNum, str))
 }
 
 func CyanPrintln(str string) {
-	fmt.Println("\033[0;" + strconv.Itoa(cyanShellColorNum) + "m" + str + "\033[0m")
+	fmt.Println(Output(cyanShellColorNum, str))
 }
 
 func RedPrintln(str string) {
-	fmt.Println("\033[0;" + strconv.Itoa(redShellColorNum) + "m" + str + "\033[0m")
+	fmt.Println(Output(redShellColorNum, str))
 }
 
 func YellowPrintln(str string) {
-	fmt.Println("\033[0;" + strconv.Itoa(yellowShellColorNum) + "m" + str + "\033[0m")
+	fmt.Println(Output(yellowShellColorNum, str))
 }
 
 func BluePrintln(str string) {
-	fmt.Println("\033[0;" + strconv.Itoa(blueShellColorNum) + "m" + str + "\033[0m")
+	fmt.Println(Output(blueShellColorNum, str))
 }
 
 func GreenPrint(str string) {
-	fmt.Print("\033[0;" + strconv.Itoa(greenShellColorNum) + "m" + str + "\033[0m")
+	fmt.Print(Output(greenShellColorNum, str))
 }
 
 func CyanPrint(str string) {
-	fmt.Print("\033[0;" + strconv.Itoa(cyanShellColorNum) + "m" + str + "\033[0m")
+	fmt.Print(Output(cyanShellColorNum, str))
 }
 
 func RedPrint(str string) {
-	fmt.Print("\033[0;" + strconv.Itoa(redShellColorNum) + "m" + str + "\033[0m")
+	fmt.Print(Output(redShellColorNum, str))
 }
 
 func YellowPrint(str string) {
-	fmt.Print("\033[0;" + strconv.Itoa(yellowShellColorNum) + "m" + str + "\033[0m")
+	fmt.Print(Output(yellowShellColorNum, str))
 }
 
 func BluePrint(str string) {
-	fmt.Print("\033[0;" + strconv.Itoa(blueShellColorNum) + "m" + str + "\033[0m")
+	fmt.Print(Output(blueShellColorNum, str))
 }
